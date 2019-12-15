48b00df7101bd34b1d09d95aa2a6161beb4d87a7
Marco Buschini <marco.buschini@gmail.com>
2019-12-15 15:19:37 +0100

    Preliminary install of the widget in the test application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Vendor } from 'widget';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'atSistemas-interview';
+  vendor: Vendor = {
+    name: 'Test Vendor',
+    description: 'This is a test vendor',
+    features: [
+      { name: 'feature 1' },
+      { name: 'feature 2' },
+      { name: 'feature 3' }
+  ]};
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { WidgetModule } from 'widget';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    WidgetModule,
     BrowserAnimationsModule
   ],
   providers: [],
