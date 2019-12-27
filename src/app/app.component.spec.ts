260ac8c33808c0c4beb44dbb5def6db84d5efab4
Marco Buschini <marco.buschini@gmail.com>
2019-12-28 00:10:18 +0100

    Typed some data that was untyped. Fixed typos. Fixed declarations, and imports. Fixed var/const.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, ComponentFixture, fakeAsync, flush } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { WidgetComponent, Vendor, ParkingSlot } from 'widget';
+import { WidgetComponent, Feature, Vendor, ParkingSlot, WidgetModule } from 'widget';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
@@ -10,48 +10,48 @@ import { of } from 'rxjs';
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
-  
+
   const dummyVendor: Vendor = {
     name: 'Test Vendor',
     description: 'This is a test vendor',
     features: [
-      { name: 'Feature 1' },
-      { name: 'Feature 2' },
-      { name: 'Feature 3' }
-    ]
+      { name: 'Feature 1' } as Feature,
+      { name: 'Feature 2' } as Feature,
+      { name: 'Feature 3' } as Feature
+    ] as Feature[]
   };
 
   const dummySlots: ParkingSlot[] = [
     {
       id: 0,
-      name: 'SLot 1',
+      name: 'Slot 1',
       features: [
         'Feature 1',
         'Feature 2'
       ]
     }
-  ]
+  ];
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        WidgetComponent,
       ],
       imports: [
         MatCardModule,
         MatDividerModule,
         MatListModule,
         HttpClientTestingModule,
+        WidgetModule,
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     fixture.componentInstance.widget = TestBed.createComponent(WidgetComponent).componentInstance;
 
-    let vendorSpy = jest.spyOn(fixture.componentInstance.widget.service, 'getVendorFeatures')
+    const vendorSpy = jest.spyOn(fixture.componentInstance.widget.service, 'getVendorFeatures')
       .mockReturnValue(of(dummyVendor.features));
-    let slotsSpy = jest.spyOn(fixture.componentInstance.widget.service, 'getParkingSlots')
+    const slotsSpy = jest.spyOn(fixture.componentInstance.widget.service, 'getParkingSlots')
       .mockReturnValue(of(dummySlots));
 
     component = fixture.componentInstance;
