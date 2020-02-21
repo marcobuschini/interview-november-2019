dd083f52419cf821e076c217eae9f805cd7e09ca
Marco Buschini <marco.buschini@gmail.com>
2020-02-21 22:39:50 +0100

    Refactored to use WidgetModule.forRoot()

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WidgetModule } from 'widget';
 import { MatListModule } from '@angular/material/list';
 import { MatRadioModule } from '@angular/material/radio';
-import { TranslateModule, TranslateLoader, MissingTranslationHandler, MissingTranslationHandlerParams } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,25 +14,10 @@ import { HttpClient } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    WidgetModule,
+    WidgetModule.forRoot(),
     MatListModule,
     MatRadioModule,
     BrowserAnimationsModule,
-    TranslateModule.forRoot({
-      loader: {
-          provide: TranslateLoader,
-          useFactory: (http: HttpClient) => new TranslateHttpLoader(http, '/assets/i18n/', '.json'),
-          deps: [HttpClient]
-      },
-      missingTranslationHandler: {
-        provide: MissingTranslationHandler,
-        useValue: {
-          handle(params: MissingTranslationHandlerParams) {
-            return '[MISSING]: ' + params.key;
-          }
-        } as MissingTranslationHandler
-      }
-    })
   ],
   providers: [],
   bootstrap: [AppComponent]
